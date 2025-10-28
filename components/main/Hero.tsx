00c7901f2f3f1d91e021d4e86f9112acf9364273
rootanvir <tanvir.22464751@gmail.com>
2025-10-28 13:21:17 +0600

    hero secrtion & resources import

---
+++ components/main/Hero.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import HeroContent from '../sub/HeroContent';
+
+interface Props {
+  
+}
+
+const Hero: FC<Props> = ({  }) => {
+  return (
+    <div className='relative flex flex-col h-full w-full'>
+        <video autoPlay muted loop 
+            className='rotate-180 absolute top-[-340px] left-0 -z[1] w-full h-full object-cover'
+        >
+            <source src="/blackhole.webm" type='video/webm'>
+            
+            </source>
+
+        </video>
+        <HeroContent />
+    </div>
+  );
+};
+
+export default Hero;
